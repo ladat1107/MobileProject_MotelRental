ffd7c9de0bfa02019c64a40b5db17e051dd1d6a8
wasabiQuoc <wasabi.together@gmail.com>
2024-03-17 10:16:54 +0700

    Thay doi avatar(giao dien)

---
+++ app/src/main/java/com/motel/mobileproject_motelrental/ThayDoiMatKhau.java
@@ -0,0 +1,13 @@
+package com.motel.mobileproject_motelrental;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThayDoiMatKhau extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.thaydoiavatar);
+    }
+}
