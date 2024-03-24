789224f10b62f857638238b6119a6392db738c4c
MinhDuong <luceattran@gmail.com>
2024-03-24 19:03:22 +0700

    MB04| many pages
    Detail room, Home page, 2 Fillter

---
+++ app/src/main/java/com/motel/mobileproject_motelrental/Fillter2Activity.java
@@ -0,0 +1,14 @@
+package com.motel.mobileproject_motelrental;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Fillter2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fillter2);
+    }
+}
