0d09ecd690f40a21364bacd5519803bc0faa39ab
ladat <ladat01626362980@gmail.com>
2024-04-08 17:44:46 +0700

    MB06 | GoogleMap

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,11 @@ dependencies {
     implementation("com.google.firebase:firebase-firestore:24.11.0")
 =======
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+<<<<<<< HEAD
 >>>>>>> dd91258 (MB06 | Update gradle version 8.3.1)
+=======
+    implementation("com.google.android.libraries.places:places:3.4.0")
+>>>>>>> a8daf71 (MB06 | GoogleMap)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/motel/mobileproject_motelrental/LocationActivity.java
+++ app/src/main/java/com/motel/mobileproject_motelrental/LocationActivity.java
@@ -2,13 +2,47 @@ package com.motel.mobileproject_motelrental;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ImageView;
+
+import com.motel.mobileproject_motelrental.databinding.ActivityLocationBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LocationActivity extends AppCompatActivity {
+
+    private ActivityLocationBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_location);
+        binding = ActivityLocationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        List<String> dataList = new ArrayList<>();
+        dataList.add("Item 1");
+        dataList.add("Item 2");
+        dataList.add("Item 3");
+        dataList.add("Item 4");
+        dataList.add("Item 5");
+        dataList.add("Item 6");
+        dataList.add("Item 7");
+        dataList.add("Item 8");
+        dataList.add("Item 8");
+        dataList.add("Item 9");
+        dataList.add("Item 10");
+        dataList.add("Item 310");
+// Thêm các mục dữ liệu khác nếu cần
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, dataList);
+        binding.cmbTinh.setAdapter(adapter);
+
+
+
+
     }
 }
