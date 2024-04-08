9407c1de9d1fba2f99fd3cf53f68b144cb5252dd
ladat <ladat01626362980@gmail.com>
2024-04-08 17:53:44 +0700

    dev | hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,11 +2,8 @@ import android.databinding.tool.processing.ViewBindingErrorMessages
 
 plugins {
     id("com.android.application")
-<<<<<<< HEAD
     id("com.google.gms.google-services")
-=======
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
->>>>>>> dd91258 (MB06 | Update gradle version 8.3.1)
 }
 
 android {
@@ -41,20 +38,13 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.databinding:databinding-runtime:8.3.1")
-<<<<<<< HEAD
     implementation("com.google.firebase:firebase-firestore:24.11.0")
-=======
     implementation("com.google.android.gms:play-services-maps:18.2.0")
-<<<<<<< HEAD
->>>>>>> dd91258 (MB06 | Update gradle version 8.3.1)
-=======
     implementation("com.google.android.libraries.places:places:3.4.0")
->>>>>>> a8daf71 (MB06 | GoogleMap)
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
