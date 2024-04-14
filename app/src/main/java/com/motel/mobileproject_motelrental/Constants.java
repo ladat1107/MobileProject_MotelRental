20708303aaa8c5cb64cfa78af89bda67745f7c87
NguyênNguyên <billbatri085@gmail.com>
2024-04-14 11:16:34 +0700

    MB08|tên collection và attribute cho firebase

---
+++ app/src/main/java/com/motel/mobileproject_motelrental/Constants.java
@@ -0,0 +1,58 @@
+package com.motel.mobileproject_motelrental;
+
+public class Constants {
+    public static final String KEY_COLLECTION_USERS = "users"; // tên collection
+    public static final String KEY_NAME = "name";
+    public static final String KEY_GENDER = "gender";
+    public static final String KEY_BIRTHDAY = "day of birth";
+    public static final String KEY_HOUSE_NUMBER = "house number";
+    public static final String KEY_ADDRESS = "address";
+    public static final String KEY_EMAIL = "email";
+    public static final String KEY_PASSWORD = "password";
+    public static final String KEY_IMAGE = "image";
+    public static final String KEY_PHONE_NUMBER = "phone number";
+    public static final String KEY_FCM_TOKEN = "fcmtoken";
+    public static final String KEY_AVAILABILITY = "availability";
+    public static final String KEY_STATUS_USER = "status";
+
+    public static final String KEY_COLLECTION_MOTELS = "motels";    // tên collection
+    public static final String KEY_POST_AUTHOR = "post author";     // documentID của users
+    public static final String KEY_TITLE = "title";
+    public static final String KEY_COUNT_LIKE = "like";
+    public static final String KEY_COUNT_ROOM = "room";
+    public static final String KEY_PRICE = "price";
+    public static final String KEY_ACREAGE = "acreage";
+    public static final String KEY_CHARACTERISTIC = "characteristic";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_COUNT_BATH = "bathroom";
+    public static final String KEY_COUNT_BED = "bedroon";
+    public static final String KEY_COUNT_FRIDGE = "fridge";
+    public static final String KEY_COUNT_WASHING_MACHINE = "washing machine";
+    public static final String KEY_GARET = "garet";
+    public static final String KEY_PRICE_WIFI = "wifi";
+    public static final String KEY_PRICE_PARKING = "parking";
+    public static final String KEY_DEPOSIT_MONEY = "deposit";
+    public static final String KEY_START_TIME = "start time";
+    public static final String KEY_END_TIME = "end time";
+    public static final String KEY_TYPE_ID = "typeID";              //documentid của types
+    public static final String KEY_STATUS_MOTEL = "status";
+
+    public static final String KEY_COLLECTION_TYPES = "types"; // tên collection
+    public static final String KEY_NAME_TYPE = "type";
+
+    public static final String KEY_COLLECTION_COMMENTS = "comments"; // tên collection
+    public static final String KEY_COMMENTER = "commenter";         // documentid của users
+    public static final String KEY_COMMENT_MOTEL = "comment motel"; //documentid của motels
+    public static final String KEY_TIME_COMMENT = "time comment";
+    public static final String KEY_CONTENT_COMMENT = "content comment";
+
+    public static final String KEY_COLLECTION_LIKES = "likes"; // tên collection
+    public static final String KEY_POST_LIKER = "post liker";   //documentid của user
+    public static final String KEY_LIKED_MOTEL = "liked motel"; //documentid của motels
+    public static final String KEY_TIME_LIKE = "time like";
+
+    public static final String KEY_COLLECTION_IMAGE = "images"; // tên collection
+    public static final String KEY_MOTEL_ID = "motelID";        //documentid cu motels
+    public static final String KEY_IMAGE_MOTEL = "image motel";
+
+}
