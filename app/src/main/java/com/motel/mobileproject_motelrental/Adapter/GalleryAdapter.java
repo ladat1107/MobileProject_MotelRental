a1605cb6d1fe9dfc8bbe5e812c9344fe842d9496
ladat <ladat01626362980@gmail.com>
2024-04-15 17:15:23 +0700

    MB13 | first push

--- app/src/main/java/com/motel/mobileproject_motelrental/Adapter/GalleryAdapter.java
+++ app/src/main/java/com/motel/mobileproject_motelrental/Adapter/GalleryAdapter.java
@@ -1,72 +1,93 @@
 package com.motel.mobileproject_motelrental.Adapter;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.net.Uri;
-
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.List;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.motel.mobileproject_motelrental.CameraActivity;
 import com.motel.mobileproject_motelrental.R;
 
+import java.util.List;
 
-public class GalleryAdapter extends RecyclerView.Adapter<GalleryAdapter.ViewHolder>  {
-    private List<Uri> mediaList;
-    private Context context;
 
-    public GalleryAdapter(Context context, List<Uri> mediaList) {
-        this.context = context;
-        this.mediaList = mediaList;
+public class GalleryAdapter extends  RecyclerView.Adapter<GalleryAdapter.GalleryViewHolder>  {
+    private List<Uri> imageUris;
+    private OnItemClickListener mListener;
+    private StorageReference storageRef;
+    private Context mContext; // Add a Context variable
+
+    public interface OnItemClickListener {
+        void onItemClick(int position);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener listener) {
+        mListener = listener;
+    }
+
+    public static class GalleryViewHolder extends RecyclerView.ViewHolder {
+        public ImageView imageView;
+        public ImageView deleteButton;
+
+        public GalleryViewHolder(View itemView, final OnItemClickListener listener) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
+            deleteButton = itemView.findViewById(R.id.btnDelete);
+
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null) {
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION) {
+                            listener.onItemClick(position);
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    public GalleryAdapter(List<Uri> imageUris, OnItemClickListener listener, Context context) {
+        this.imageUris = imageUris;
+        mListener = listener;
+        mContext = context; // Assign Context from the constructor
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
     }
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.ld_custom_item_image, parent, false);
-        return new ViewHolder(view);
+    public GalleryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ld_custom_item_image, parent, false);
+        return new GalleryViewHolder(v, mListener);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, @SuppressLint("RecyclerView") int position) {
-        Uri mediaUri = mediaList.get(position);
-        Glide.with(context)
-                .load(mediaUri)
-                .into(holder.imageView);
-
-        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (position >= 0 && position < mediaList.size()) { // Kiểm tra vị trí hợp lệ
-                    mediaList.remove(position);
-                    notifyItemRemoved(position);
-                }
-            }
-        });
-
+    public void onBindViewHolder(@NonNull GalleryViewHolder holder, int position) {
+        Uri uri = imageUris.get(position);
+        holder.imageView.setImageURI(uri);
     }
 
     @Override
     public int getItemCount() {
-        return mediaList.size();
+        return imageUris.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        ImageView imageView;
-        ImageButton btnDelete;
-
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            imageView = itemView.findViewById(R.id.imageView);
-            btnDelete = itemView.findViewById(R.id.btnDelete);
-        }
+    public void deleteItem(int position) {
+        imageUris.remove(position);
+        notifyItemRemoved(position);
     }
+
 }
--- app/src/main/java/com/motel/mobileproject_motelrental/CameraActivity.java
+++ app/src/main/java/com/motel/mobileproject_motelrental/CameraActivity.java
@@ -1,13 +1,18 @@
 package com.motel.mobileproject_motelrental;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.ClipData;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -15,101 +20,156 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Base64;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.motel.mobileproject_motelrental.Adapter.GalleryAdapter;
 import com.motel.mobileproject_motelrental.databinding.ActivityCameraBinding;
 
 
 import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-public class CameraActivity extends AppCompatActivity {
-    private static final int REQUEST_CODE = 1;
+public class CameraActivity extends AppCompatActivity implements GalleryAdapter.OnItemClickListener {
+    private ActivityCameraBinding binding;
 
     private RecyclerView recyclerView;
     private GalleryAdapter adapter;
-   // private List<Uri> selectedMedia = new ArrayList<>();
-  //  private StorageReference storageReference;
-    private ActivityCameraBinding binding;
-    String encodedImage;
-    private List<Uri> selectedMedia = new ArrayList<>();
-    private GalleryAdapter galleryAdapter;
+    private List<Uri> selectedUris = new ArrayList<>();
+    private StorageReference storageRef;
 
-    // Phương thức để mở Gallery và chọn ảnh và video
-    private void openGallery() {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-        intent.setType("image/*, video/*");
-        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
-        startActivityForResult(intent, REQUEST_CODE);
-    }
-
-    // Xử lý kết quả trả về từ Gallery
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-            ClipData clipData = data.getClipData();
+    private final ActivityResultLauncher<Intent> pickMedia = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
+        if (result.getResultCode() == RESULT_OK && result.getData() != null) {
+            ClipData clipData = result.getData().getClipData();
             if (clipData != null) {
-                // Nếu chọn nhiều ảnh và video
                 for (int i = 0; i < clipData.getItemCount(); i++) {
-                    selectedMedia.add(clipData.getItemAt(i).getUri());
+                    selectedUris.add(clipData.getItemAt(i).getUri());
                 }
             } else {
-                // Nếu chỉ chọn một ảnh hoặc video
-                selectedMedia.add(data.getData());
+                selectedUris.add(result.getData().getData());
             }
-            galleryAdapter.notifyDataSetChanged();
+            adapter.notifyDataSetChanged(); // Update RecyclerView
         }
-    }
+    });
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
         binding = ActivityCameraBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        binding.tvThuVien.setOnClickListener(new View.OnClickListener() {
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+
+        // Sửa đổi ở đây: Truyền 'this' (Context của hoạt động) vào Adapter
+        adapter = new GalleryAdapter(selectedUris, this, this);
+        recyclerView.setAdapter(adapter);
+
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+
+        findViewById(R.id.tvThuVien).setOnClickListener(v -> openGalleryForMedia());
+        binding.btnTiepTuc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openGallery();
+                uploadFiles();
             }
         });
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
-        galleryAdapter = new GalleryAdapter(this, selectedMedia);
-        recyclerView.setLayoutManager(new GridLayoutManager(this, 3)); // 3 items trên mỗi hàng
-        recyclerView.setAdapter(galleryAdapter);
     }
-    private final ActivityResultLauncher<Intent> pickImage = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result -> {
-        if (result.getResultCode() == RESULT_OK){
-            if(result.getData() != null) {
-                Uri imageUri = result.getData().getData();
-                try {
-                    InputStream inputStream = getContentResolver().openInputStream(imageUri);
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
 
-                    encodedImage = endcodeImage(bitmap);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
+    private void openGalleryForMedia() {
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*"); // Cho phép tất cả các loại tệp
+        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+        pickMedia.launch(intent);
+    }
+
+    private void uploadFiles() {
+        if (selectedUris.isEmpty()) {
+            Toast.makeText(this, "Không có tệp nào được chọn!", Toast.LENGTH_SHORT).show();
+            return;
         }
-    });
 
-    private String endcodeImage(Bitmap bitmap) {
-        int previewWidth = 150;
-        int previewHeight = bitmap.getHeight() * previewWidth / bitmap.getWidth();
-        Bitmap previewBitmap = Bitmap.createScaledBitmap(bitmap, previewWidth, previewHeight, false);
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        previewBitmap.compress(Bitmap.CompressFormat.JPEG, 50, byteArrayOutputStream);
-        byte[] bytes = byteArrayOutputStream.toByteArray();
-        return Base64.encodeToString(bytes, Base64.DEFAULT);
+        for (Uri uri : selectedUris) {
+            StorageReference fileRef = storageRef.child("uploads/" + System.currentTimeMillis());
+            fileRef.putFile(uri)
+                    .addOnSuccessListener(taskSnapshot -> {
+                        // Lấy URL của nội dung đã tải lên
+                        Toast.makeText(CameraActivity.this, "Tải lên thành công", Toast.LENGTH_SHORT).show();
+                    })
+                    .addOnFailureListener(e -> {
+                        // Xử lý việc tải lên không thành công
+                        Toast.makeText(CameraActivity.this, "Tải lên thất bại: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    });
+        }
+
+        // Xóa danh sách selectedUris sau khi tải lên
+        selectedUris.clear();
+        adapter.notifyDataSetChanged();
     }
 
-
-
+    @Override
+    public void onItemClick(int position) {
+        adapter.deleteItem(position);
+    }
 }
+//    private void openGalleryForMedia() {
+//        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+//        intent.setType("*/*");
+//        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+//        pickMedia.launch(intent);
+//    }
+//
+//    private void uploadFiles() {
+//        for (Uri uri : selectedUris) {
+//            StorageReference fileRef = storageRef.child("Image/ImageMotel/" + System.currentTimeMillis());
+//            UploadTask uploadTask = fileRef.putFile(uri);
+//
+//            uploadTask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+//                @Override
+//                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+//                    if (!task.isSuccessful()) {
+//                        throw task.getException();
+//                    }
+//                    return fileRef.getDownloadUrl();
+//                }
+//            }).addOnCompleteListener(task -> {
+//                if (task.isSuccessful()) {
+//                    Uri downloadUri = task.getResult();
+//                    if (downloadUri != null) {
+//                        Log.d("CameraActivity", "File uploaded: " + downloadUri.toString());
+//                    }
+//                } else {
+//                    Toast.makeText(CameraActivity.this, "Upload failed: " + task.getException(), Toast.LENGTH_SHORT).show();
+//                }
+//            });
+//        }
+//    }
+     /* private FirebaseFirestore firestore;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+
+    private static final int REQUEST_CODE = 1;
+    private GalleryAdapter adapter;
+    private List<Uri> selectedUris = new ArrayList<>();*/
+
 
