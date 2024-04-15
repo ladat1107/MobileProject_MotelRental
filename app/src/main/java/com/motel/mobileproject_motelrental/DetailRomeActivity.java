41905a36249247841d1db6386e9e056e11f9baea
MinhDuong <luceattran@gmail.com>
2024-04-15 13:09:08 +0700

    MB14 | Một chút FireBase

--- app/src/main/java/com/motel/mobileproject_motelrental/DetailRomeActivity.java
+++ app/src/main/java/com/motel/mobileproject_motelrental/DetailRomeActivity.java
@@ -1,13 +1,20 @@
 package com.motel.mobileproject_motelrental;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 import com.motel.mobileproject_motelrental.Adapter.CommentAdapter;
 import com.motel.mobileproject_motelrental.Adapter.ImageAdapter;
 import com.motel.mobileproject_motelrental.Item.CommentItem;
@@ -21,6 +28,8 @@ public class DetailRomeActivity extends AppCompatActivity {
     private ActivityDetailRomeBinding binding;
     private List<Image> imageList;
     private ImageAdapter adapter;
+    private String TAG = "DetailRomeActivity";
+
 
     boolean isFavorite = false;
     @Override
@@ -48,6 +57,27 @@ public class DetailRomeActivity extends AppCompatActivity {
                 } else {
                     binding.btnYeuThich.setImageResource(R.drawable.img_love);
                     isFavorite = true;
+                }
+            }
+        });
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("motels").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        if(document.getId().equals("6CPkXvPbYlD09EFNnp1A")) {
+                            Log.d(TAG, document.getId() + " => " + document.getData());
+                            binding.txtTitle.setText(document.getString("title"));
+                            binding.txtLove.setText(document.getString("like") + " lượt yêu thích");
+                            binding.txtAddress.setText(document.getString("motel number") + ", " + document.getString("ward") + ", " + document.getString("district") + ", " + document.getString("city"));
+                            binding.txtCharac.setText(document.getString("characteristics"));
+                            binding.txtPrice.setText(document.getString("price"));
+                        }
+                    }
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
                 }
             }
         });
