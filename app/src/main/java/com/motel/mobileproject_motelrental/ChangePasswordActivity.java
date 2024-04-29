0ccb0dfef3cc5c8b9cc85d741009bc42e9e250eb
wasabiQuoc <wasabi.together@gmail.com>
2024-04-29 16:29:56 +0700

    MB36 | fix resetpass o trang thay doi mk

--- app/src/main/java/com/motel/mobileproject_motelrental/ChangePasswordActivity.java
+++ app/src/main/java/com/motel/mobileproject_motelrental/ChangePasswordActivity.java
@@ -11,22 +11,13 @@ import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 public class ChangePasswordActivity extends AppCompatActivity {
@@ -91,7 +82,7 @@ public class ChangePasswordActivity extends AppCompatActivity {
         btnQuenMatKhau.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ResetPasswordActivity.class);
+                Intent intent = new Intent(getApplicationContext(), EnterMailActivity.class);
                 startActivity(intent);
             }
         });
